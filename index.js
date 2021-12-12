2ae44f661df40a8395725fbce28d119d4b4f25a7
Oleksii Ustinov <ustinovoleksij@gmail.com>
2021-12-12 19:31:42 +0200

    Added function

---
+++ index.js
@@ -0,0 +1,11 @@
+function pushToArray() {
+const array = [];
+while(true) {
+    const answer = prompt('Enter your word');
+    if (answer === null||answer === '') {
+        array.pop();
+    }else {
+        array.push(answer);
+    }console.log();
+}
+}
